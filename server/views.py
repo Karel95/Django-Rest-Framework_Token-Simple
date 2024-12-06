134ee961f5c115126431e6c261103eb7840074c3
Karel <karelh2207@gmail.com>
2024-12-06 15:01:05 -0300

    updating

--- server/views.py
+++ server/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 
 
@@ -24,7 +25,16 @@ def register(request):
 
 @api_view(['POST'])
 def login(request):
-  return Response({})
+  user = get_object_or_404(User, email = request.data['email'])
+  
+  if not user.check_password(request.data['password']):
+    return Response({'error': 'Invalid password'}, status = status.HTTP_400_BAD_REQUEST)
+  
+  token, created = Token.objects.get_or_create(user=user)
+  
+  serializer = UserSerializer(instance=user)
+  
+  return Response({'token': token.key, 'user': serializer.data}, status = status.HTTP_200_OK)
 
 @api_view(['POST'])
 def profile(request):
